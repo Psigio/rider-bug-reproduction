0ea5e67ae791c19dfd8561ddab192281d318a5e7
Jon <Psigio@users.noreply.github.com>
2022-03-10 10:20:12 +0000

    Add reference to Serilog and call static method

--- Application/Program.cs
+++ Application/Program.cs
@@ -7,6 +7,7 @@ namespace Application
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Serilog.Log.Logger.Information("Hello World from Logger!");
         }
     }
 }
